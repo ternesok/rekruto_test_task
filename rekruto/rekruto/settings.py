0032611a5103a1644bf1ff292e737f556d0986e6
ternesok <ternesok@gmail.com>
2023-04-24 00:00:47 +0300

    first commit

--- rekruto/rekruto/settings.py
+++ rekruto/rekruto/settings.py
@@ -11,7 +11,10 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+from dotenv import load_dotenv
+import os
 
+load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -19,12 +22,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-r7&xmz&*=njqn9&vsj3vbykh!z8z7+j^8&j@mis5c932rfgi@5'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', '*']
+ALLOWED_HOSTS = ['roflan00.pythonanywhere.com', 'www.roflan00.pythonanywhere.com', '127.0.0.1']
 
 # Application definition
 
